a93b72f3d2c7b53fa205a2ef5ba85b8e810b40d4
sushmitha <20b01a1248@svecw.edu.in>
2022-10-30 13:28:56 +0530

    added packages express ejs firebase-admin to repo

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Initiating repo")
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res)=>{
+	res.send("Hello world! This is Sushmitha !!");
+});
+
+app.listen(port,()=>{
+	console.log(`You are in port number ${port}`);
+});
